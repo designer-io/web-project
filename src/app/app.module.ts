2134bcdefc295450c47d73287ac4dea7ba96c3bb
unknown <vibhavraj88@gmail.com>
2020-04-10 01:31:21 +0530

    category,footer done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./components/nav/nav.component";
 import { HomeBannerComponent } from "./components/home-banner/home-banner.component";
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeBannerComponent],
+  declarations: [AppComponent, NavComponent, HomeBannerComponent, CategoriesComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
